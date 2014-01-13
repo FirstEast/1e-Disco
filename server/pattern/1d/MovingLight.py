20288a78059dbe244c941a6aaa42e795a4489123
Joey Rafidi <jfidi731@gmail.com>
2014-01-13 10:17:36 -0500

    pattern cleanup

--- server/pattern/1d/MovingLight.py
+++ server/pattern/1d/MovingLight.py
@@ -10,15 +10,19 @@ class MovingLightPattern(Pattern):
     'rate': 60
   }
 
-  DEVICES = ['goodale']
+  DEVICES = ['goodale', 'bemis']
 
   def __init__(self, beat, params):
     Pattern.__init__(self, beat, params);
     self.startTime = time.time() * 1000;
 
   def render(self, device):
-    frameCount = int((time.time() * 1000 - self.startTime) / float(1000 / self.params['rate'])) % GOODALE_WIDTH
+    if device.deviceName == 'goodale':
+      limiter = GOODALE_WIDTH
+    else if device.deviceName == 'bemis':
+      limiter = BEMIS_WIDTH
+    frameCount = int((time.time() * 1000 - self.startTime) / float(1000 / self.params['rate'])) % limiter
     frame = [[0, 0, 0]] * frameCount
     frame = frame + [self.params['color'].getRGBValues()]
-    frame = frame + [[0, 0, 0]] * (GOODALE_WIDTH - 1 - frameCount)
+    frame = frame + [[0, 0, 0]] * (limiter - 1 - frameCount)
     return [frame]
