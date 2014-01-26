a1018d6b8926ee24bf9310ba187ce84b25aa9dd9
NiBr <NiBr@MIT.edu>
2014-01-25 19:43:55 -0500

    Created Diamond maker

--- server/pattern/mixer/transform.py
+++ server/pattern/mixer/transform.py
@@ -28,6 +28,27 @@ class RingsMaker(Pattern):
       for y in range(-self.params['CenterY'], device.height - self.params['CenterY'])
       for x in range(-self.params['CenterX'], device.width - self.params['CenterX'])])
     return ret
+  
+class DiamondMaker(Pattern):
+  DEFAULT_PARAMS = {
+    'Pattern': 'sliding_rainbow.json',
+    'Size': 20,
+    'Rate': 5,
+    'CenterX': 24,
+    'CenterY': 12
+  }
+
+  def paramUpdate(self):
+    self.pattern = loadSavedPatternFromFilename(self.beat, self.params['Pattern'])
+    if 'Rate' in self.pattern.params: self.pattern.params['Rate'] *= self.params['Rate']
+
+  def render(self, device):
+    array = list(self.pattern.render(MOCK_DEVICES['goodale']).getdata())
+    ret = Image.new('RGB', (device.width, device.height))
+    ret.putdata([array[int(len(array) * (abs(y) + abs(x)) / (self.params['Size'])) % len(array)]
+      for y in range(-self.params['CenterY'], device.height - self.params['CenterY'])
+      for x in range(-self.params['CenterX'], device.width - self.params['CenterX'])])
+    return ret
 
 class RadarMaker(Pattern):
   DEFAULT_PARAMS = {
