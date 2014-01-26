771a60c98c603097a3dd483933abf098530c06c5
Joey Rafidi <jrafidi@mit.edu>
2014-01-26 17:59:48 -0500

    rpi hax

--- goodale/GoodaleLights.py
+++ goodale/GoodaleLights.py
@@ -4,11 +4,21 @@ from twisted.web.server import Site
 from twisted.web.static import File
 from sockets.RenderSocket import *
 
+import os
+
+USB_PREFIX = '/media/'
+
 DISCO_SERVER_HOST = "localhost"
 DISCO_SERVER_PORT = 8123
 
 if __name__ == '__main__':
+  usb = os.walk(USB_PREFIX).next()[1][0]
+  config = USB_PREFIX + usb + '/config.txt'
+  host = ''
+  with open(config, 'r') as f:
+    host = f.readline()
+  
   # Setup render socket
-  point = TCP4ClientEndpoint(reactor, DISCO_SERVER_HOST, DISCO_SERVER_PORT)
+  point = TCP4ClientEndpoint(reactor, host, DISCO_SERVER_PORT)
   d = point.connect(RenderSocketFactory())
   reactor.run()
